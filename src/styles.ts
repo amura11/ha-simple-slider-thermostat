662c13f524f8c9597feccefdecb7fb8c4c5b7bdd
AndrewQ <amura.cxg@gmail.com>
2023-11-28 05:09:55 +0000

    In progress commit, partially functional code

---
+++ src/styles.ts
@@ -0,0 +1,4 @@
+import { CSSResult, css, unsafeCSS } from "lit";
+import styles from "../node_modules/nouislider/dist/nouislider.min.css";
+
+export const SliderStyles: CSSResult = unsafeCSS(styles);
---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+import { HassEntityBase } from 'home-assistant-js-websocket';
+
+declare module "*.css" {
+    const css: string;
+    export default css;
+}
